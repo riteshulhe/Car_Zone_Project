a2c81b3b4e980fe6fca437901c6d365fb805fa2d
Ritesh <riteshulhe3035@gmail.com>
2021-09-30 16:38:16 +0530

    completed Team Model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Team
 
-# Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width= "40" style="border-radius: 50px;"/>'.format(obj.photo.url))
+
+    image_tag.short_description = 'Image'
+
+    list_display = ('id','image_tag', 'first_name','last_name', 'designation', 'created_date')
+    list_display_links = ('id','image_tag','first_name', 'designation')
+    list_filter = ('designation',)
+    search_fields = ('first_name', 'last_name')
+    #list_editable = ('last_name',)
+
+admin.site.register(Team,TeamAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
-
+from .models import Team
 def home(request):
-    return render(request,'pages/home.html', name = 'home')
+    teams = Team.objects.all()
+    data = { 'teams': teams, }
+    return render(request,'pages/home.html', data)
+
+
+def about(request):
+    teams = Team.objects.all()
+    data = {'teams': teams,}
+    return render(request, 'pages/about.html', data)
+
+def services(request):
+    return render(request,'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
